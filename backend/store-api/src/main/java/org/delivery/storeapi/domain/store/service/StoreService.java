7023fa749a45b3e1e7cee90892d57cf7f4be2e3a
gyo1227 <gyo1227@naver.com>
2024-07-10 05:48:28 +0900

    fact: 스토어 오픈 상태 변경 구현

--- backend/store-api/src/main/java/org/delivery/storeapi/domain/store/service/StoreService.java
+++ backend/store-api/src/main/java/org/delivery/storeapi/domain/store/service/StoreService.java
@@ -8,6 +8,7 @@ import org.delivery.db.store.StoreRepository;
 import org.delivery.db.store.enums.StoreStatus;
 import org.delivery.storeapi.domain.store.controller.model.StoreRegisterRequest;
 import org.delivery.storeapi.domain.store.controller.model.StoreResponse;
+import org.delivery.storeapi.domain.store.controller.model.StoreStatusUpdateRequest;
 import org.delivery.storeapi.domain.storeuser.model.StoreUserSession;
 import org.springframework.stereotype.Service;
 
@@ -42,6 +43,18 @@ public class StoreService {
                 .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "StoreRegisterRequest is Null"));
 
         var savedEntity = storeRepository.save(entity);
+        var response = storeConverter.toResponse(savedEntity);
+
+        return response;
+    }
+
+    public StoreResponse updateStatus(StoreStatusUpdateRequest request, StoreUserSession storeUserSession) {
+        var storeEntity = storeRepository.findFirstByIdAndOwnerId(request.getStoreId(), storeUserSession.getId())
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT));
+
+        storeEntity.setStatus(request.getStatus());
+
+        var savedEntity = storeRepository.save(storeEntity);
         var response = storeConverter.toResponse(savedEntity);
 
         return response;
