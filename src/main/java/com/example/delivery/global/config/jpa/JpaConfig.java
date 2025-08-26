ccd4b2b34f888defaa618af94111df5cbf8ed1ac
gyo1227 <gyo1227@naver.com>
2025-08-26 19:23:42 +0900

    feat: security config 추가, jpa config 수정

--- src/main/java/com/example/delivery/global/config/jpa/JpaConfig.java
+++ src/main/java/com/example/delivery/global/config/jpa/JpaConfig.java
@@ -1,7 +1,9 @@
 package com.example.delivery.global.config.jpa;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
 @EnableJpaAuditing
 public class JpaConfig {
 }
---
+++ src/main/java/com/example/delivery/global/config/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.delivery.global.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/api/auth/**").anonymous()
+                        .anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
